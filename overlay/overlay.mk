367ae111d2c8a62a332f687d82dab85e7841d030
Chris Trotman <solarnz@teameos.org>
2011-12-24 18:44:22 +1300

    Generic sysrw/sysro and rootro/rootrw commands for remounting filesystems.
    Change-Id: I0d6ead661e0ed3f8865ccd61836cb37742d6ed87

--- overlay/overlay.mk
+++ overlay/overlay.mk
@@ -1,3 +1,7 @@
 
 PRODUCT_COPY_FILES += \
-    vendor/eos/overlay/bin/sysinit:system/bin/sysinit
+    vendor/eos/overlay/bin/sysinit:system/bin/sysinit \
+    vendor/eos/overlay/bin/sysrw:system/bin/sysrw \
+    vendor/eos/overlay/bin/sysro:system/bin/sysro \
+    vendor/eos/overlay/bin/rootrw:system/bin/rootrw \
+    vendor/eos/overlay/bin/rootro:system/bin/rootro \
