e99ae54cca6ca70a282cb71c893512a443e619e7
Chris Trotman <solarnz@teameos.org>
2012-04-25 11:16:14 +1000

    Bring in the addon.d support from CM9 for backing up / restoring files on flash (3/3)
    Change-Id: Ia50bc77477dd4c875d5fe15626176d0ee8cc0514

--- overlay/overlay.mk
+++ overlay/overlay.mk
@@ -6,3 +6,5 @@ PRODUCT_COPY_FILES += \
     vendor/eos/overlay/bin/rootrw:system/bin/rootrw \
     vendor/eos/overlay/bin/rootro:system/bin/rootro \
     vendor/eos/overlay/etc/init.d/02overclock:system/etc/init.d/02overclock \
+    vendor/eos/overlay/bin/backuptool.functions:system/bin/backuptool.functions \
+    vendor/eos/overlay/bin/backuptool.sh:system/bin/backuptool.sh \
