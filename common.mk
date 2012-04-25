29726e8a2adfa25d9503018780c3e4462bec6d38
Chris Trotman <solarnz@teameos.org>
2012-04-25 22:24:42 +1000

    Eos: Remove Titanium Backup
    Unfortuantly TitaniumBackup have moved to a different method for their
    libtitanium.so library that cannot be not read from /system/lib. This means
    that ultimately we have had to remove this useful application from the rom.
    
    Change-Id: Idfa18529e4fadf2916f3e1f515febdaf27441e43

--- common.mk
+++ common.mk
@@ -36,7 +36,7 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     vendor/eos/proprietary/terminalemulator/libjackpal-androidterm3.so:system/lib/libjackpal-androidterm3.so \
 
-$(call inherit-product-if-exists, vendor/eos/proprietary/titaniumbackup/titaniumbackup.mk)
+#$(call inherit-product-if-exists, vendor/eos/proprietary/titaniumbackup/titaniumbackup.mk)
 $(call inherit-product-if-exists, vendor/eos/overlay/overlay.mk)
 DEVICE_PACKAGE_OVERLAYS += vendor/eos/package_overlays
 
