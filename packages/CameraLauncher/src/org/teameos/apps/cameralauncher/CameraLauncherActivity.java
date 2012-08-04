2989999df382307f772b0f34f4425b886a4b49e5
Chris Trotman <solarnz@teameos.org>
2012-08-04 13:01:06 +1000

    CameraLauncher: Change to use the same code as the lockscreen.
    This is to avoid using the somewhat buggy method of calling the camera
    intent directly.
    
    Change-Id: I20d7945076ec0bdbc9205cb2f52c52b0d343bed3

--- packages/CameraLauncher/src/org/teameos/apps/cameralauncher/CameraLauncherActivity.java
+++ packages/CameraLauncher/src/org/teameos/apps/cameralauncher/CameraLauncherActivity.java
@@ -4,13 +4,15 @@ import android.app.Activity;
 import android.content.ComponentName;
 import android.content.Intent;
 import android.os.Bundle;
+import android.provider.MediaStore;
 
 public class CameraLauncherActivity extends Activity{
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        Intent cameraIntent = new Intent("android.intent.action.MAIN");
-        cameraIntent.setComponent(new ComponentName("com.google.android.gallery3d",
-                "com.android.camera.Camera"));
+        Intent cameraIntent = new Intent(MediaStore.INTENT_ACTION_STILL_IMAGE_CAMERA);
+        cameraIntent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK
+                        | Intent.FLAG_ACTIVITY_SINGLE_TOP
+                        | Intent.FLAG_ACTIVITY_CLEAR_TOP);
         startActivity(cameraIntent);
         finish();
     };
