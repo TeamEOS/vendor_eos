fd07a7a9f1178997aef1909912f460a95ce4fc33
Kenneth R. Crudup <kenny@panix.com>
2012-10-02 20:55:03 -0700

    Enable ZRAM swap
    Should have no effect if ZRAM not enabled in kernel
    
    Change-Id: If7de4966ea96531dcd0af17d4e24441cf465beb8

--- filesystem_overlay/overlay.mk
+++ filesystem_overlay/overlay.mk
@@ -5,6 +5,7 @@ PRODUCT_COPY_FILES += \
     vendor/eos/filesystem_overlay/bin/sysro:system/bin/sysro \
     vendor/eos/filesystem_overlay/bin/rootrw:system/bin/rootrw \
     vendor/eos/filesystem_overlay/bin/rootro:system/bin/rootro \
+    vendor/eos/filesystem_overlay/etc/init.d/01zram-swap:system/etc/init.d/01zram-swap \
     vendor/eos/filesystem_overlay/etc/init.d/02overclock:system/etc/init.d/02overclock \
     vendor/eos/filesystem_overlay/etc/init.d/03eosSettings:system/etc/init.d/03eosSettings \
     vendor/eos/filesystem_overlay/bin/backuptool.functions:system/bin/backuptool.functions \
@@ -12,3 +13,4 @@ PRODUCT_COPY_FILES += \
     vendor/eos/filesystem_overlay/addon.d/99-eos-hosts.sh:system/addon.d/99-eos-hosts.sh \
     vendor/eos/filesystem_overlay/media/LMprec_508.emd:system/media/LMprec_508.emd \
     vendor/eos/filesystem_overlay/media/PFFprec_600.emd:system/media/PFFprec_600.emd \
+
