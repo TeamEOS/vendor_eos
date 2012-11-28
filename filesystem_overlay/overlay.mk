ec5a4d639c9cbde8270c862d55b87d8269515b4b
Randall Rushing <randall.rushing@gmail.com>
2012-11-28 03:01:57 -0800

    Move zoneinfo fix to eos vendor as it appears to affect all devices
    Change-Id: Idec67f172e3c062fcb2116f4876a10a56013dbfb

--- filesystem_overlay/overlay.mk
+++ filesystem_overlay/overlay.mk
@@ -13,4 +13,10 @@ PRODUCT_COPY_FILES += \
     vendor/eos/filesystem_overlay/addon.d/99-eos-hosts.sh:system/addon.d/99-eos-hosts.sh \
     vendor/eos/filesystem_overlay/media/LMprec_508.emd:system/media/LMprec_508.emd \
     vendor/eos/filesystem_overlay/media/PFFprec_600.emd:system/media/PFFprec_600.emd \
+# Proper zoneinfo files
+# don't know why the proper ZoneinfoDB classes aren't updated yet
+PRODUCT_COPY_FILES += \
+    vendor/eos/filesystem_overlay/usr/share/zoneinfo/zoneinfo.dat:system/usr/share/zoneinfo/zoneinfo.dat \
+    vendor/eos/filesystem_overlay/usr/share/zoneinfo/zoneinfo.idx:system/usr/share/zoneinfo/zoneinfo.idx \
+    vendor/eos/filesystem_overlay/usr/share/zoneinfo/zoneinfo.version:system/usr/share/zoneinfo/zoneinfo.version
 
