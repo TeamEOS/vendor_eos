17edcd94e478206ab9639c58ea55231a1012cd25
Randall Rushing <randall.rushing@gmail.com>
2012-12-10 13:47:15 -0800

    Revert "Merge "Temp Fix for lockscreen unresponcive when pin/pattern set" into jellybean-4.2"
    This reverts commit 9cccdd70d7b5833fd2c9920c33373263ca048d69, reversing
    changes made to 1b714b3624295c36dd03489a4bb933994983514f.

--- filesystem_overlay/overlay.mk
+++ filesystem_overlay/overlay.mk
@@ -8,7 +8,6 @@ PRODUCT_COPY_FILES += \
     vendor/eos/filesystem_overlay/etc/init.d/01zram-swap:system/etc/init.d/01zram-swap \
     vendor/eos/filesystem_overlay/etc/init.d/02overclock:system/etc/init.d/02overclock \
     vendor/eos/filesystem_overlay/etc/init.d/03eosSettings:system/etc/init.d/03eosSettings \
-    vendor/eos/filesystem_overlay/etc/init.d/04pintempfix:system/etc/init.d/04pintempfix \
     vendor/eos/filesystem_overlay/bin/backuptool.functions:system/bin/backuptool.functions \
     vendor/eos/filesystem_overlay/bin/backuptool.sh:system/bin/backuptool.sh \
     vendor/eos/filesystem_overlay/addon.d/99-eos-hosts.sh:system/addon.d/99-eos-hosts.sh \
