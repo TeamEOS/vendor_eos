bdf97f9fdc74066cec8c9b0c78baa6f2e9a8f44d
Randall Rushing <randall.rushing@gmail.com>
2013-01-04 02:53:55 -0800

    Rewrite Softkey color and glow code (1 of 2)
    Change-Id: I340f3f14f1563fc919da974469160857cac7a2ed

--- packages/EosControlCenter/src/org/eos/controlcenter/SoftKeyActions.java
+++ packages/EosControlCenter/src/org/eos/controlcenter/SoftKeyActions.java
@@ -6,6 +6,7 @@ import android.content.Context;
 import android.os.Bundle;
 import android.preference.CheckBoxPreference;
 import android.preference.Preference;
+import android.preference.PreferenceScreen;
 import android.provider.Settings;
 
 import org.teameos.jellybean.settings.EOSConstants;
@@ -13,11 +14,32 @@ import org.teameos.jellybean.settings.EOSConstants;
 public class SoftKeyActions extends ActionFragment
         implements Preference.OnPreferenceChangeListener {
 
+    Context mContext;
+    ContentResolver mResolver;
+
     CheckBoxPreference mEnable;
     CheckBoxPreference mMenuPersist;
+    ColorPreference mBackColor;
+    ColorPreference mHomeColor;
+    ColorPreference mRecentColor;
+    ColorPreference mMenuColor;
+    ColorPreference mBackGlowColor;
+    ColorPreference mHomeGlowColor;
+    ColorPreference mRecentGlowColor;
+    ColorPreference mMenuGlowColor;
+    Preference mBackColorDef;
+    Preference mHomeColorDef;
+    Preference mRecentColorDef;
+    Preference mMenuColorDef;
+    Preference mBackGlowColorDef;
+    Preference mHomeGlowColorDef;
+    Preference mRecentGlowColorDef;
+    Preference mMenuGlowColorDef;
 
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        mContext = (Context) getActivity();
+        mResolver = mContext.getContentResolver();
         addPreferencesFromResource(R.xml.softkey_settings);
 
         mEnable = (CheckBoxPreference) findPreference("eos_interface_softkey_enable_feature");
@@ -35,46 +57,67 @@ public class SoftKeyActions extends ActionFragment
                 EOSConstants.SYSTEMUI_SOFTKEY_MENU_PERSIST, 0) == 1);
         mMenuPersist.setOnPreferenceChangeListener(this);
 
-        // back softkey and glow colors
-        ((MultiColorPreference) findPreference("eos_softkey_color_back"))
-                .setRestorePref((Preference) findPreference("eos_interface_softkey_back_default_key_color"));
-        ((MultiColorPreference) findPreference("eos_softkey_glow_color_back"))
-                .setRestorePref((Preference) findPreference("eos_interface_softkey_back_default_glow_color"));
+        mBackColor = (ColorPreference) findPreference("eos_softkey_color_back");
+        mBackColor.setProviderTarget(EOSConstants.SYSTEMUI_NAVKEY_BACK_KEY_COLOR, EOSConstants.SYSTEMUI_NAVBAR_SOFTKEY_DEFAULT);
+        mBackColorDef = (Preference) findPreference("eos_interface_softkey_back_default_key_color");
 
-        // home softkey and glow colors
-        ((MultiColorPreference) findPreference("eos_softkey_color_home"))
-                .setRestorePref((Preference) findPreference("eos_interface_softkey_home_default_key_color"));
-        ((MultiColorPreference) findPreference("eos_softkey_glow_color_home"))
-                .setRestorePref((Preference) findPreference("eos_interface_softkey_home_default_glow_color"));
+        mBackGlowColor = (ColorPreference) findPreference("eos_softkey_glow_color_back");
+        mBackGlowColor.setProviderTarget(EOSConstants.SYSTEMUI_NAVKEY_BACK_GLOW_COLOR, EOSConstants.SYSTEMUI_NAVBAR_SOFTKEY_DEFAULT);
+        mBackGlowColorDef = (Preference) findPreference("eos_interface_softkey_back_default_glow_color");
 
-        // recent softkey and glow colors
-        ((MultiColorPreference) findPreference("eos_softkey_color_recent"))
-                .setRestorePref((Preference) findPreference("eos_interface_softkey_recent_default_key_color"));
-        ((MultiColorPreference) findPreference("eos_softkey_glow_color_recent"))
-                .setRestorePref((Preference) findPreference("eos_interface_softkey_recent_default_glow_color"));
+        mHomeColor = (ColorPreference) findPreference("eos_softkey_color_home");
+        mHomeColor.setProviderTarget(EOSConstants.SYSTEMUI_NAVKEY_HOME_KEY_COLOR, EOSConstants.SYSTEMUI_NAVBAR_SOFTKEY_DEFAULT);
+        mHomeColorDef = (Preference) findPreference("eos_interface_softkey_home_default_key_color");
 
-        // menu softkey and glow colors
-        ((MultiColorPreference) findPreference("eos_softkey_color_menu"))
-                .setRestorePref((Preference) findPreference("eos_interface_softkey_menu_default_key_color"));
-        ((MultiColorPreference) findPreference("eos_softkey_glow_color_menu"))
-                .setRestorePref((Preference) findPreference("eos_interface_softkey_menu_default_glow_color"));
-        checkColorUri();
+        mHomeGlowColor = (ColorPreference) findPreference("eos_softkey_glow_color_home");
+        mHomeGlowColor.setProviderTarget(EOSConstants.SYSTEMUI_NAVKEY_HOME_GLOW_COLOR, EOSConstants.SYSTEMUI_NAVBAR_SOFTKEY_DEFAULT);
+        mHomeGlowColorDef = (Preference) findPreference("eos_interface_softkey_home_default_glow_color");
+
+        mRecentColor = (ColorPreference) findPreference("eos_softkey_color_recent");
+        mRecentColor.setProviderTarget(EOSConstants.SYSTEMUI_NAVKEY_RECENT_KEY_COLOR, EOSConstants.SYSTEMUI_NAVBAR_SOFTKEY_DEFAULT);
+        mRecentColorDef = (Preference) findPreference("eos_interface_softkey_recent_default_key_color");
+
+        mRecentGlowColor = (ColorPreference) findPreference("eos_softkey_glow_color_recent");
+        mRecentGlowColor.setProviderTarget(EOSConstants.SYSTEMUI_NAVKEY_RECENT_GLOW_COLOR, EOSConstants.SYSTEMUI_NAVBAR_SOFTKEY_DEFAULT);
+        mRecentGlowColorDef = (Preference) findPreference("eos_interface_softkey_recent_default_glow_color");
+
+        mMenuColor = (ColorPreference) findPreference("eos_softkey_color_menu");
+        mMenuColor.setProviderTarget(EOSConstants.SYSTEMUI_NAVKEY_MENU_KEY_COLOR, EOSConstants.SYSTEMUI_NAVBAR_SOFTKEY_DEFAULT);
+        mMenuColorDef = (Preference) findPreference("eos_interface_softkey_menu_default_key_color");
+
+        mMenuGlowColor = (ColorPreference) findPreference("eos_softkey_glow_color_menu");
+        mMenuGlowColor.setProviderTarget(EOSConstants.SYSTEMUI_NAVKEY_MENU_GLOW_COLOR, EOSConstants.SYSTEMUI_NAVBAR_SOFTKEY_DEFAULT);
+        mMenuGlowColorDef = (Preference) findPreference("eos_interface_softkey_menu_default_glow_color");
     }
 
-    // check for null uri values before preference is selected
-    // move here instead of the preference itself
-    private void checkColorUri() {
-        // test glow color and softkey color
-        ContentResolver resolver = ((Context) getActivity()).getContentResolver();
-        String filler = "-1|-1|-1|-1|";
-        String key = Settings.System.getString(resolver, "eos_systemui_navkey_color");
-        String glow = Settings.System.getString(resolver, "eos_systemui_navglow_color");
-        if (key == null || key.equals("") || key.equals(" ")) {
-            Settings.System.putString(resolver, "eos_systemui_navkey_color", filler);
+    @Override
+    public boolean onPreferenceTreeClick(PreferenceScreen screen, Preference pref) {
+        if (pref.equals(mBackColorDef)) {
+            Settings.System.putInt(mResolver, EOSConstants.SYSTEMUI_NAVKEY_BACK_KEY_COLOR, EOSConstants.SYSTEMUI_NAVKEY_COLOR_DEF);
+            return true;
+        } else if (pref.equals(mBackGlowColorDef)) {
+            Settings.System.putInt(mResolver, EOSConstants.SYSTEMUI_NAVKEY_BACK_GLOW_COLOR, EOSConstants.SYSTEMUI_NAVKEY_COLOR_DEF);
+            return true;
+        } else if (pref.equals(mHomeColorDef)) {
+            Settings.System.putInt(mResolver, EOSConstants.SYSTEMUI_NAVKEY_HOME_KEY_COLOR, EOSConstants.SYSTEMUI_NAVKEY_COLOR_DEF);
+            return true;
+        } else if (pref.equals(mHomeGlowColorDef)) {
+            Settings.System.putInt(mResolver, EOSConstants.SYSTEMUI_NAVKEY_HOME_GLOW_COLOR, EOSConstants.SYSTEMUI_NAVKEY_COLOR_DEF);
+            return true;
+        } else if (pref.equals(mRecentColorDef)) {
+            Settings.System.putInt(mResolver, EOSConstants.SYSTEMUI_NAVKEY_RECENT_KEY_COLOR, EOSConstants.SYSTEMUI_NAVKEY_COLOR_DEF);
+            return true;
+        } else if (pref.equals(mRecentGlowColorDef)) {
+            Settings.System.putInt(mResolver, EOSConstants.SYSTEMUI_NAVKEY_RECENT_GLOW_COLOR, EOSConstants.SYSTEMUI_NAVKEY_COLOR_DEF);
+            return true;
+        } else if (pref.equals(mMenuColorDef)) {
+            Settings.System.putInt(mResolver, EOSConstants.SYSTEMUI_NAVKEY_MENU_KEY_COLOR, EOSConstants.SYSTEMUI_NAVKEY_COLOR_DEF);
+            return true;
+        } else if (pref.equals(mMenuGlowColorDef)) {
+            Settings.System.putInt(mResolver, EOSConstants.SYSTEMUI_NAVKEY_MENU_GLOW_COLOR, EOSConstants.SYSTEMUI_NAVKEY_COLOR_DEF);
+            return true;
         }
-        if (glow == null || glow.equals("") || glow.equals(" ")) {
-            Settings.System.putString(resolver, "eos_systemui_navglow_color", filler);
-        }
+        return false;
     }
 
     @Override
