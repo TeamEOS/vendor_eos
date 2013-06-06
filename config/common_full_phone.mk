9ef56e4132bf07fd18b31f45c9f831feddbf2c7c
Shareef Ali <shareefalis@cyanogenmod.org>
2013-06-06 18:52:10 -0500

    cm: World Phone support
    * combine apns
    * leave cdma/gsm files intact but empty until the calls
    * are removed from devices cm.mk
    
    Change-Id: I0686ef4ee1fe9b34ac9beaaa2bd43555234f4f6d
    
    Conflicts:
    	config/cdma.mk
    	config/common_full_phone.mk
    	config/common_mini_phone.mk
    	config/gsm.mk
    	prebuilt/common/etc/apns-conf-cdma.xml
    	prebuilt/common/etc/apns-conf.xml
    
    Change-Id: I6c549df9376f429ec93f95bcb81954a005300da0

--- config/common_full_phone.mk
+++ config/common_full_phone.mk
@@ -3,3 +3,15 @@ $(call inherit-product, vendor/eos/config/common_full.mk)
 
 PRODUCT_PACKAGES += \
   Mms
+
+# World APN list
+PRODUCT_COPY_FILES += \
+    vendor/eos/prebuilt/common/etc/apns-conf.xml:system/etc/apns-conf.xml
+
+# World SPN overrides list
+PRODUCT_COPY_FILES += \
+    vendor/eos/prebuilt/common/etc/spn-conf.xml:system/etc/spn-conf.xml
+
+# SIM Toolkit
+PRODUCT_PACKAGES += \
+    Stk
