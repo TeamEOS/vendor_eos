62903f9a74751cdec812f2ce5fff8c2dd998abfa
Randall Rushing <randall.rushing@gmail.com>
2013-07-14 01:45:36 -0700

    Controls for crt on/off and screen on when charging
    Summary: Change-Id: Ia7e934122744f7c5a3d030d435ea4bd8c877e0da
    
    Test Plan: merge and win
    
    Reviewers: Mustaavalkosta, michyprima, rlasalle15, randall.rushing, synergy
    
    Differential Revision: http://review.codefirex.com/D223

--- packages/CfxSettings/src/com/cfx/settings/SystemSettings.java
+++ packages/CfxSettings/src/com/cfx/settings/SystemSettings.java
@@ -1,11 +1,23 @@
 package com.cfx.settings;
 
+import org.codefirex.utils.CFXConstants;
+
 import android.os.Bundle;
-import android.preference.PreferenceFragment;
+import android.preference.CheckBoxPreference;
 import android.preference.Preference;
+import android.provider.Settings;
 
 public class SystemSettings extends CFXPreferenceFragment {
+	private static final String SCREEN_PLUG = "cfx_system_power_unplug_screen_off";
+	private static final String CRT_OFF = "cfx_system_power_crt_screen_off";
+	private static final String CRT_ON = "cfx_system_power_crt_screen_on";
+
 	OnActivityRequestedListener mListener;
+
+	private CheckBoxPreference mScreenCharging;
+	private CheckBoxPreference mCrtOff;
+	private CheckBoxPreference mCrtOn;
+	private boolean isCrtOffChecked = false;
 
 	public static SystemSettings newInstance() {
 		return new SystemSettings();
@@ -18,6 +30,57 @@ public class SystemSettings extends CFXPreferenceFragment {
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		addPreferencesFromResource(R.xml.system_settings);
+
+		// respect device default configuration
+		// true fades while false animates
+		boolean electronBeamFadesConfig = getResources().getBoolean(
+				com.android.internal.R.bool.config_animateScreenLights);
+
+		// use this to enable/disable crt on feature
+		// crt only works if crt off is enabled
+		// total system failure if only crt on is enabled
+		isCrtOffChecked = Settings.System.getInt(mResolver,
+				CFXConstants.SYSTEM_POWER_ENABLE_CRT_OFF,
+				electronBeamFadesConfig ? 0 : 1) == 1;
+
+		mCrtOff = (CheckBoxPreference) findPreference(CRT_OFF);
+		mCrtOff.setChecked(isCrtOffChecked);
+		mCrtOff.setOnPreferenceChangeListener(new Preference.OnPreferenceChangeListener() {
+
+			@Override
+			public boolean onPreferenceChange(Preference preference,
+					Object newValue) {
+				isCrtOffChecked = ((Boolean) newValue).booleanValue();
+				Settings.System.putInt(mResolver,
+						CFXConstants.SYSTEM_POWER_ENABLE_CRT_OFF,
+						(isCrtOffChecked ? 1 : 0));
+				// if crt off gets turned off, crt on gets turned off and
+				// disabled
+				if (!isCrtOffChecked) {
+					Settings.System.putInt(mResolver,
+							CFXConstants.SYSTEM_POWER_ENABLE_CRT_ON, 0);
+					mCrtOn.setChecked(false);
+				}
+				mCrtOn.setEnabled(isCrtOffChecked);
+				return true;
+			}
+		});
+
+		mCrtOn = (CheckBoxPreference) findPreference(CRT_ON);
+		mCrtOn.setChecked(Settings.System.getInt(mResolver,
+				CFXConstants.SYSTEM_POWER_ENABLE_CRT_ON, 0) == 1);
+		mCrtOn.setEnabled(isCrtOffChecked);
+		mCrtOn.setOnPreferenceChangeListener(new Preference.OnPreferenceChangeListener() {
+
+			@Override
+			public boolean onPreferenceChange(Preference preference,
+					Object newValue) {
+				Settings.System.putInt(mResolver,
+						CFXConstants.SYSTEM_POWER_ENABLE_CRT_ON,
+						((Boolean) newValue).booleanValue() ? 1 : 0);
+				return true;
+			}
+		});
 	}
 
 }
