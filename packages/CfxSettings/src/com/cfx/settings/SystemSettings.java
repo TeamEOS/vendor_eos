a1564dd4f382f138b446640616018e50669bef96
Randall Rushing <randall.rushing@gmail.com>
2013-07-14 17:21:03 -0700

    Controls for volume features
    Summary: Change-Id: Iad3af6d787a277451555cbf0c8045e35d316458b
    
    Test Plan: goes with D226
    
    Reviewers: Mustaavalkosta, michyprima, rlasalle15, randall.rushing, synergy
    
    Reviewed By: synergy
    
    Differential Revision: http://review.codefirex.com/D227

--- packages/CfxSettings/src/com/cfx/settings/SystemSettings.java
+++ packages/CfxSettings/src/com/cfx/settings/SystemSettings.java
@@ -4,16 +4,23 @@ import org.codefirex.utils.CFXConstants;
 
 import android.os.Bundle;
 import android.preference.CheckBoxPreference;
+import android.preference.ListPreference;
 import android.preference.Preference;
 import android.provider.Settings;
 
 public class SystemSettings extends CFXPreferenceFragment {
+	private static final String VOLUME_OVERLAY = "cfx_system_volume_panel_controls_key";
+	private static final String VOLUME_BUTTON_SOUND = "cfx_system_volume_hard_button_sounds";
+	private static final String VOLUME_LINK = "cfx_system_volume_link_notification_key";
 	private static final String SCREEN_PLUG = "cfx_system_power_unplug_screen_off";
 	private static final String CRT_OFF = "cfx_system_power_crt_screen_off";
 	private static final String CRT_ON = "cfx_system_power_crt_screen_on";
 
 	OnActivityRequestedListener mListener;
 
+	private ListPreference mVolumePanelStyle;
+	private CheckBoxPreference mVolumeKeySounds;
+	private CheckBoxPreference mVolumeNotificationLink;
 	private CheckBoxPreference mScreenCharging;
 	private CheckBoxPreference mCrtOff;
 	private CheckBoxPreference mCrtOn;
@@ -30,6 +37,55 @@ public class SystemSettings extends CFXPreferenceFragment {
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		addPreferencesFromResource(R.xml.system_settings);
+
+		mVolumePanelStyle = (ListPreference) findPreference(VOLUME_OVERLAY);
+		mVolumePanelStyle.setValue(String.valueOf(Settings.System.getInt(
+				mResolver, Settings.System.MODE_VOLUME_OVERLAY,
+				Settings.System.VOLUME_OVERLAY_EXPANDABLE)));
+		mVolumePanelStyle
+				.setOnPreferenceChangeListener(new Preference.OnPreferenceChangeListener() {
+
+					@Override
+					public boolean onPreferenceChange(Preference preference,
+							Object newValue) {
+						int val = Integer.parseInt((String) newValue);
+						Settings.System.putInt(mResolver,
+								Settings.System.MODE_VOLUME_OVERLAY, val);
+						return true;
+					}
+				});
+
+		mVolumeNotificationLink = (CheckBoxPreference) findPreference(VOLUME_LINK);
+		mVolumeNotificationLink.setChecked(Settings.System.getInt(mResolver,
+				Settings.System.VOLUME_LINK_NOTIFICATION, 1) == 1);
+		mVolumeNotificationLink
+				.setOnPreferenceChangeListener(new Preference.OnPreferenceChangeListener() {
+
+					@Override
+					public boolean onPreferenceChange(Preference preference,
+							Object newValue) {
+						Settings.System.putInt(mResolver,
+								Settings.System.VOLUME_LINK_NOTIFICATION,
+								((Boolean) newValue).booleanValue() ? 1 : 0);
+						return true;
+					}
+				});
+
+		mVolumeKeySounds = (CheckBoxPreference) findPreference(VOLUME_BUTTON_SOUND);
+		mVolumeKeySounds.setChecked(Settings.System.getInt(mResolver,
+				Settings.System.VOLUME_ADJUST_SOUNDS_ENABLED, 1) == 1);
+		mVolumeKeySounds
+				.setOnPreferenceChangeListener(new Preference.OnPreferenceChangeListener() {
+
+					@Override
+					public boolean onPreferenceChange(Preference preference,
+							Object newValue) {
+						Settings.System.putInt(mResolver,
+								Settings.System.VOLUME_ADJUST_SOUNDS_ENABLED,
+								((Boolean) newValue).booleanValue() ? 1 : 0);
+						return true;
+					}
+				});
 
 		// respect device default configuration
 		// true fades while false animates
