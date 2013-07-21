111242824aa6f2ca0a7a0a4a4a15824da5f749cb
Randall Rushing <randall.rushing@gmail.com>
2013-07-21 02:17:57 -0700

    SmallSystemStub fake app for overriding product packages We need this to compile xoom. As you may notice, this completely guts the build. Perhaps adding a board flag to enable deodex would be more prudent.
    Summary: Change-Id: I869a6f08ef7e38b0baf5592a76b72e277d5a9133
    
    Test Plan: it works, barely
    
    Reviewers: synergy, randall.rushing
    
    Reviewed By: synergy
    
    Differential Revision: http://review.codefirex.com/D256

---
+++ packages/SmallSystemStub/Android.mk
@@ -0,0 +1,77 @@
+LOCAL_PATH:= $(call my-dir)
+include $(CLEAR_VARS)
+
+LOCAL_MODULE_TAGS := optional
+LOCAL_SRC_FILES := $(call all-java-files-under, src)
+
+LOCAL_PACKAGE_NAME := SmallSystemStub
+LOCAL_OVERRIDES_PACKAGES := \
+    HoloSpiralWallpaper \
+    MagicSmokeWallpapers \
+    NoiseField \
+    Galaxy4 \
+    VisualizationWallpapers \
+    PhaseBeam
+
+LOCAL_OVERRIDES_PACKAGES += \
+    LiveWallpapers \
+    LiveWallpapersPicker \
+    Calculator \
+    Calendar \
+    DeskClock \
+    Apollo \
+    Basic \
+    VideoEditor \
+    VoiceDialer \
+    SoundRecorder \
+    ZeroXBenchmark \
+    SpareParts \
+    LockClock \
+    Development \
+    Email \
+    WAPPushManager \
+    MusicFX \
+    Email2 \
+    Exchange2 \
+    QuickSearchBox
+
+LOCAL_OVERRIDES_PACKAGES += \
+    LatinIME \
+    PinyinIME \
+    OpenWnn \
+    libWnnEngDic \
+    libWnnJpnDic \
+    libwnndict \
+    libfwdlockengine
+
+# Extra tools in cfX
+LOCAL_OVERRIDES_PACKAGES += \
+    openvpn \
+    e2fsck \
+    mke2fs \
+    tune2fs \
+    bash \
+    vim \
+    nano \
+    htop \
+    powertop \
+    lsof \
+    systembinsh
+
+# Openssh
+LOCAL_OVERRIDES_PACKAGES += \
+    scp \
+    sftp \
+    ssh \
+    sshd \
+    sshd_config \
+    ssh-keygen \
+    start-ssh
+
+# rsync
+LOCAL_OVERRIDES_PACKAGES += \
+    rsync
+
+LOCAL_SDK_VERSION := current
+
+include $(BUILD_PACKAGE)
