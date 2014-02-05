77aebba99002795565c5f55be10b068e11446ccc
Randall Rushing <randall.rushing@gmail.com>
2014-02-05 00:16:33 -0800

    Remove all depricated main bt conf
    Summary: These are all depricated, along with the system/bluetooth project
    
    Test Plan: merge in conjunction with removing system/bluetooth from manifest
    
    Reviewers: synergy
    
    Differential Revision: http://review.codefirex.com/D389

--- config/common_full_phone.mk
+++ config/common_full_phone.mk
@@ -3,7 +3,3 @@ $(call inherit-product, vendor/cfx/config/common_full.mk)
 
 PRODUCT_PACKAGES += \
   Mms
-
-# BT config
-PRODUCT_COPY_FILES += \
-    system/bluetooth/data/main.conf:system/etc/bluetooth/main.conf
--- config/common_full_tablet_wifionly.mk
+++ config/common_full_tablet_wifionly.mk
@@ -1,10 +1,6 @@
 # Inherit common cfX stuff
 $(call inherit-product, vendor/cfx/config/common_full.mk)
 
-# BT config
-PRODUCT_COPY_FILES += \
-    system/bluetooth/data/main.nonsmartphone.conf:system/etc/bluetooth/main.conf
-
 ifeq ($(TARGET_SCREEN_WIDTH) $(TARGET_SCREEN_HEIGHT),$(space))
     PRODUCT_COPY_FILES += \
         vendor/cfx/prebuilt/common/bootanimation/720.zip:system/media/bootanimation.zip
