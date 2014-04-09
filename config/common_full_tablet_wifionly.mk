263e9fa23adef514dc950852554930c75daed314
Christopher N. Hesse <raymanfx@gmail.com>
2014-04-09 11:39:10 +0200

    Fix dead path references

--- config/common_full_tablet_wifionly.mk
+++ config/common_full_tablet_wifionly.mk
@@ -3,5 +3,5 @@ $(call inherit-product, vendor/eos/config/common_full.mk)
 
 ifeq ($(TARGET_SCREEN_WIDTH) $(TARGET_SCREEN_HEIGHT),$(space))
     PRODUCT_COPY_FILES += \
-        vendor/cfx/prebuilt/common/bootanimation/720.zip:system/media/bootanimation.zip
+        vendor/eos/prebuilt/common/bootanimation/720.zip:system/media/bootanimation.zip
 endif
