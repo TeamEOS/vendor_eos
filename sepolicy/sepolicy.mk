fc2a8e094a520af1e4213e755ad7e46ce9a28bfd
Ricardo Cerqueira <ricardo@cyngn.com>
2014-11-09 17:20:54 +0000

    selinux: Add rules for the audit daemon
    Conflicts:
    
    	sepolicy/file_contexts
    	sepolicy/sepolicy.mk
    
    Change-Id: I050a9ef39d58d2592d880d225d45eb64d8a40b7b

--- sepolicy/sepolicy.mk
+++ sepolicy/sepolicy.mk
@@ -1,16 +1,20 @@
 #
 # This policy configuration will be used by all products that
+# inherit from CM
 #
 
 BOARD_SEPOLICY_DIRS += \
-    vendor/eos/sepolicy
+    vendor/cm/sepolicy
 
 BOARD_SEPOLICY_UNION += \
     file.te \
     file_contexts \
-    fs_use \
     genfs_contexts \
-    installd.te \
     seapp_contexts \
+    auditd.te \
+    installd.te \
+    netd.te \
+    system.te \
+    ueventd.te \
     vold.te \
     mac_permissions.xml
