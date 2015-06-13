5f524d674ad4aa5e2e11e88f96d75abd09fa56d3
Randall Rushing <randall.rushing@gmail.com>
2015-06-12 19:47:37 -0700

    Prebuilts: bring in navigation shared and static jars (2 of 4)
    finalize navigation abstraction and jar it up
    
    Change-Id: I4f38610ab296924ec8594ed5016ff60eb6222ac5

---
+++ prebuilt/common/framework/Android.mk
@@ -0,0 +1,18 @@
+LOCAL_PATH:= $(call my-dir)
+
+include $(CLEAR_VARS)
+LOCAL_SRC_FILES := org.teameos.navigation.jar
+LOCAL_MODULE_TAGS := optional          
+LOCAL_MODULE := org.teameos.navigation
+LOCAL_CERTIFICATE := platform
+LOCAL_MODULE_CLASS := JAVA_LIBRARIES
+LOCAL_MODULE_SUFFIX := $(COMMON_JAVA_PACKAGE_SUFFIX)
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+
+LOCAL_PREBUILT_STATIC_JAVA_LIBRARIES := \
+    org.teameos.navigation-static:org.teameos.navigation-static.jar
+
+include $(BUILD_MULTI_PREBUILT)
+
