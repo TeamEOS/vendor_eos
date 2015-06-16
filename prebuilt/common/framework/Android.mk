81a8962d02ade0e92628a13cb02184a0af31303a
Randall Rushing <randall.rushing@gmail.com>
2015-06-16 02:10:39 -0700

    Prebuilts: bring in trail-drawing library for Fling (2 of 2)
    Change-Id: Icac41ce7363075f6c15d061438e485c7f21f088c
    source: https://github.com/Orange-OpenSource/trail-drawing

--- prebuilt/common/framework/Android.mk
+++ prebuilt/common/framework/Android.mk
@@ -12,7 +12,8 @@ include $(BUILD_PREBUILT)
 include $(CLEAR_VARS)
 
 LOCAL_PREBUILT_STATIC_JAVA_LIBRARIES := \
-    org.teameos.navigation-static:org.teameos.navigation-static.jar
+    org.teameos.navigation-static:org.teameos.navigation-static.jar \
+    trail-drawing:trail-core-lib-1.0.4-SNAPSHOT.jar
 
 include $(BUILD_MULTI_PREBUILT)
 
