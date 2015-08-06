9d89d56503b5adfbafa4ac8584b4dad996477336
Randall Rushing <randall.rushing@gmail.com>
2015-08-05 23:26:42 -0700

    Navigation: shared library is not needed at this time
    We will require a shared library when IPC/Binder/Service is needed
    
    Change-Id: I75358cdd9b0e85accd10ff4f55cd5400e8a1d160

--- prebuilt/common/framework/Android.mk
+++ prebuilt/common/framework/Android.mk
@@ -1,14 +1,4 @@
 LOCAL_PATH:= $(call my-dir)
-
-include $(CLEAR_VARS)
-LOCAL_SRC_FILES := org.teameos.navigation.jar
-LOCAL_MODULE_TAGS := optional          
-LOCAL_MODULE := org.teameos.navigation
-LOCAL_CERTIFICATE := platform
-LOCAL_MODULE_CLASS := JAVA_LIBRARIES
-LOCAL_MODULE_SUFFIX := $(COMMON_JAVA_PACKAGE_SUFFIX)
-include $(BUILD_PREBUILT)
-
 include $(CLEAR_VARS)
 
 LOCAL_PREBUILT_STATIC_JAVA_LIBRARIES := \
